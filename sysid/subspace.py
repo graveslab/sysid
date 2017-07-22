c3198d63aced6062d13c9991dc112580623ebf0a
Brandon Curtis <brandon.curtis@gmail.com>
2017-07-22 03:41:30 -0700

    python3 compatibility; fixed subspace ID

--- sysid/subspace.py
+++ sysid/subspace.py
@@ -4,7 +4,8 @@ This module performs subspace system identification.
 It enforces that matrices are used instead of arrays
 to avoid dimension conflicts.
 """
-import pylab as pl
+import numpy as np
+import scipy.linalg
 from . import ss
 
 __all__ = ['subspace_det_algo1', 'prbs', 'nrms']
@@ -16,18 +17,18 @@ def block_hankel(data, f):
     Create a block hankel matrix.
     f : number of rows
     """
-    data = pl.matrix(data)
-    assert len(data.shape) == 2
-    n = data.shape[1] - f
-    return pl.matrix(pl.hstack([
-        pl.vstack([data[:, i+j] for i in range(f)])
+    data = np.matrix(data)
+    assert len(data.shape) == 2   # make sure it's a 2D matrix
+    n = data.shape[1] - f         # n is the number of timepoints minus the lookahead 
+    return np.matrix(np.hstack([
+        np.vstack([data[:, i+j] for i in range(f)])
         for j in range(n)]))
 
 def project(A):
     """
     Creates a projection matrix onto the rowspace of A.
     """
-    A = pl.matrix(A)
+    A = np.matrix(A)
     return  A.T*(A*A.T).I*A
 
 def project_perp(A):
@@ -35,8 +36,8 @@ def project_perp(A):
     Creates a projection matrix onto the space perpendicular to the
     rowspace of A.
     """
-    A = pl.matrix(A)
-    I = pl.matrix(pl.eye(A.shape[1]))
+    A = np.matrix(A)
+    I = np.matrix(np.eye(A.shape[1]))
     P = project(A)
     return  I - P
 
@@ -47,7 +48,7 @@ def project_oblique(B, C):
     proj_B_perp = project_perp(B)
     return proj_B_perp*(C*proj_B_perp).I*C
 
-def subspace_det_algo1(y, u, f, p, s_tol, dt):
+def subspace_det_algo1(y, u, f, p, s_tol, dt, nx=None):
     """
     Subspace Identification for deterministic systems
     algorithm 1 from (1)
@@ -73,29 +74,42 @@ def subspace_det_algo1(y, u, f, p, s_tol, dt):
     assert p > 1
 
     # setup matrices
-    y = pl.matrix(y)
+    # these are column vectors, so the number of rows is the number of inputs
+    y = np.matrix(y)
     n_y = y.shape[0]
-    u = pl.matrix(u)
+    
+    u = np.matrix(u)
     n_u = u.shape[0]
-    w = pl.vstack([y, u])
+    
+    w = np.vstack([y, u])
     n_w = w.shape[0]
+    
+    ### BC
+    #print("u:{}".format(u))
+    #print("n_y:{}, n_u:{}".format(n_y,n_u))
 
     # make sure the input is column vectors
     assert y.shape[0] < y.shape[1]
     assert u.shape[0] < u.shape[1]
-
+    
+    # build block Hankel matrices for the inputs u, outputs y, and w
     W = block_hankel(w, f + p)
     U = block_hankel(u, f + p)
+    #print("U:{}".format(U)) ## BC 
+    #print("U.shape:{}".format(U.shape))
     Y = block_hankel(y, f + p)
 
     W_p = W[:n_w*p, :]
     W_pp = W[:n_w*(p+1), :]
 
     Y_f = Y[n_y*f:, :]
-    U_f = U[n_y*f:, :]
+    U_f = U[n_u*f:, :] # THIS IS BREAKING - messed up subscript
 
     Y_fm = Y[n_y*(f+1):, :]
     U_fm = U[n_u*(f+1):, :]
+    
+    #print("U_f: {}".format(U_f))
+    #print("W_p: {}".format(W_p))
 
     # step 1, calculate the oblique projections
     #------------------------------------------
@@ -111,22 +125,34 @@ def subspace_det_algo1(y, u, f, p, s_tol, dt):
     # given: W1 O_i W2 = G_i Xd_p
     # want to solve for G_i, but know product, and not Xd_p
     # so can only find Xd_p up to a similarity transformation
-    W1 = pl.matrix(pl.eye(O_i.shape[0]))
-    W2 = pl.matrix(pl.eye(O_i.shape[1]))
-    U0, s0, VT0 = pl.svd(W1*O_i*W2)  #pylint: disable=unused-variable
+    W1 = np.matrix(np.eye(O_i.shape[0]))
+    W2 = np.matrix(np.eye(O_i.shape[1]))
+    U0, s0, VT0 = scipy.linalg.svd(W1*O_i*W2)  #pylint: disable=unused-variable
+    # BC: U0, s0, VT0 are returned by pylab.svd()
 
     # step 3, determine the order by inspecting the singular
     #------------------------------------------
     # values in S and partition the SVD accordingly to obtain U1, S1
     #print s0
-    n_x = pl.find(s0/s0.max() > s_tol)[-1] + 1
+    # BC: what is S, S0, S1?
+    # BC: n_x is the order!  this is determined automatically!
+    # BC: n_x is one plus the first singular value that's larger than s_tol
+    #print("s0:{}".format(s0))
+    
+    n_x = np.argwhere(s0/s0.max() > s_tol)[-1][0] + 1 # BC ADDED
+    if nx: # BC ADDED
+        print("order {} calculated, but order {} specified!".format(n_x, nx)) # BC ADDED
+        n_x = nx # BC ADDED
+    print("n_x:{}, n_u:{}, n_y:{}".format(n_x,n_u,n_y)) # BC ADDED
+    
     U1 = U0[:, :n_x]
-    # S1 = pl.matrix(pl.diag(s0[:n_x]))
+    
+    # S1 = np.matrix(np.diag(s0[:n_x]))
     # VT1 = VT0[:n_x, :n_x]
 
     # step 4, determine Gi and Gim
     #------------------------------------------
-    G_i = W1.I*U1*pl.matrix(pl.diag(pl.sqrt(s0[:n_x])))
+    G_i = W1.I*U1*np.matrix(np.diag(np.sqrt(s0[:n_x])))
     G_im = G_i[:-n_y, :] # check
 
     # step 5, determine Xd_ip and Xd_p
@@ -141,31 +167,40 @@ def subspace_det_algo1(y, u, f, p, s_tol, dt):
     Y_ii = Y[n_y*p:n_y*(p+1), :]
     U_ii = U[n_u*p:n_u*(p+1), :]
 
-    a_mat = pl.matrix(pl.vstack([Xd_ip, Y_ii]))
-    b_mat = pl.matrix(pl.vstack([Xd_i, U_ii]))
+    a_mat = np.matrix(np.vstack([Xd_ip, Y_ii]))
+    b_mat = np.matrix(np.vstack([Xd_i, U_ii]))
     ss_mat = a_mat*b_mat.I
     A_id = ss_mat[:n_x, :n_x]
     B_id = ss_mat[:n_x, n_x:]
-    assert B_id.shape[0] == n_x
-    assert B_id.shape[1] == n_u
+    assert B_id.shape[0] == n_x # number of states
+    assert B_id.shape[1] == n_u # number of inputs
     C_id = ss_mat[n_x:, :n_x]
-    assert C_id.shape[0] == n_y
-    assert C_id.shape[1] == n_x
+    assert C_id.shape[0] == n_y # number of outputs
+    assert C_id.shape[1] == n_x # number of states
     D_id = ss_mat[n_x:, n_x:]
-    assert D_id.shape[0] == n_y
-    assert D_id.shape[1] == n_u
-
-    if pl.matrix_rank(C_id) == n_x:
+    assert D_id.shape[0] == n_y # number of outputs
+    assert D_id.shape[1] == n_u # number of inputs
+    
+    #print("ss_mat:{}".format(ss_mat))
+    #print("A_id:{}".format(A_id))
+    #print("B_id:{}".format(B_id))
+    #print("C_id:{}".format(C_id))
+    #print("D_id:{}".format(D_id))
+    
+    if np.ndim(C_id) == n_x:
         T = C_id.I # try to make C identity, want it to look like state feedback
     else:
-        T = pl.matrix(pl.eye(n_x))
+        T = np.matrix(np.eye(n_x))
 
-    Q_id = pl.zeros((n_x, n_x))
-    R_id = pl.zeros((n_y, n_y))
+    Q_id = np.zeros((n_x, n_x))
+    R_id = np.zeros((n_y, n_y))
+    
+    # we're doing this locally
     sys = ss.StateSpaceDiscreteLinear(
         A=T.I*A_id*T, B=T.I*B_id, C=C_id*T, D=D_id,
-        Q=Q_id, R=R_id, dt=dt)
+        Q=Q_id, R=R_id, dt=dt)  
     return sys
+
 
 
 def nrms(data_fit, data_true):
@@ -173,17 +208,17 @@ def nrms(data_fit, data_true):
     Normalized root mean square error.
     """
     # root mean square error
-    rms = pl.mean(pl.norm(data_fit - data_true, axis=0))
+    rms = np.mean(np.linalg.norm(data_fit - data_true, axis=0))
 
     # normalization factor is the max - min magnitude, or 2 times max dist from mean
-    norm_factor = 2*pl.norm(data_true - pl.mean(data_true, axis=1), axis=0).max()
+    norm_factor = 2*np.linalg.norm(data_true - np.mean(data_true, axis=1), axis=0).max()
     return (norm_factor - rms)/norm_factor
 
 def prbs(n):
     """
     Pseudo random binary sequence.
     """
-    return pl.where(pl.rand(n) > 0.5, 0, 1)
+    return np.where(np.random.rand(n) > 0.5, 0, 1)
 
 
 # vim: set et fenc=utf-8 ft=python  ff=unix sts=4 sw=4 ts=4 :
